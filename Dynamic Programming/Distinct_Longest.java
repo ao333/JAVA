99d2bff05c2e27f45631c679b9de1464dc8d9d99
ao333 <ao333@Ao.localdomain>
2018-03-26 15:43:59 +0100

    s

--- Dynamic Programming/Distinct_Longest.java
+++ Dynamic Programming/Distinct_Longest.java
@@ -13,14 +13,15 @@ public class Distinct_Longest {
         System.out.println("Longest Length of a " + LONGEST(a));
     }
 
-    /* 1 a) The time complexity of this solution is O(N^2) since there are 2 for-loops of which the second one is nested.
-    While j=i+1 can reduce the number of iterations for the second loop, both are still proportional to the array size,
-    so the overall time complexity does not change. The space complexity is O(1), since no new array is created.
+    /* 1a) The worst case time complexity of this solution is O(N^2) since there is a nested for-loop. While j=i+1 can
+    reduce the number of iterations for the second loop, both are still proportional to the array size, so the overall
+    time complexity does not change. The space complexity is O(1), since only i and j are created.
 
     An alternative solution would be to sort the array first, and check index i against i+1 of the sorted array for
     duplicates. Bubble, insertion, and selection sort all fit the O(N^2) time and O(1) space criteria as the sorting is
     done on the same array. The comparison for-loop afterwards would make the overall time complexity O(N^2+N).
     However, since N^2, is the highest order, the overall time complexity can be simplified to O(N^2) */
+
     private static boolean DISTINCT(int a[]) {
         for (int i=0; i<a.length-1; i++){
             for (int j=i+1; j<a.length; j++){
@@ -30,12 +31,13 @@ public class Distinct_Longest {
         return true;
     }
 
-    /* 1 b) Distinct uses a Hash Set. The structure, which is similar to a hash table that disallows duplicate keys, was
-    chosen for its suitability to this question. The contains method check against a no-duplicate set in constant time.
-    The add method builds that set in constant time. However, since we still need to iterate through the list to compare
-    and build the list, the overall function is still proportional to the array size, therefore O(N). While the single
-    for-loop and hashing reduces time complexity, the creation of a new hash set proportional to the array size
+    /* 1b) Distinct uses a Hash Set. The structure, which is similar to a hash table but disallows duplicate keys, was
+    chosen for its suitability to this question. The contains method check against a set in constant time. The add method
+    builds that set in amortized O(1) time because it may need to rehash and enlarge the table. However, since we still
+    need to iterate through the list to compare and build the list, the overall function is still proportional to the
+    array size, therefore O(N). While the single for-loop and hashing reduces time complexity, the creation of a new hash
     increases its space complexity to O(N). This is the trade-off compared to implementations in 1 a). */
+
     private static boolean DISTINCT2(int a[]) {
         Set<Integer> hs = new HashSet<>();
         for (int i : a) {
@@ -53,6 +55,7 @@ public class Distinct_Longest {
     array size. The other 2 for-loops makes the time complexity O(N^2+2N). However, since the highest order is N^2, our
     big O remains O(N^2). The creation of a longest[] array holding the longest increasing sequence increases the space
     complexity to O(N). */
+
     private static int LONGEST(int a[]) {
         int size = a.length;
         int longest[] = new int[size];
